0688b7af030fcb5e57e1f2033a796e55165d5ec9
Sundas-Razzaq <sundasr621@gmail.com>
2026-06-27 16:29:55 +0500

    Configured backend for Vercel deployment

--- Infra-Backend/server.js
+++ Infra-Backend/server.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
+
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
+// Connect Database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
--- Infra-Backend/src/models/inspection.model.js
+++ Infra-Backend/src/models/inspection.model.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const InspectionSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    originalImage: { type: String, required: true },
-    processedImage: { type: String },
-    crackCount: { type: Number, default: 0 },
-    crackSeverity: { type: String },
-    confidenceScore: { type: Number },
-    crackType: { type: String },
-    recommendation: { type: String },
-    reportPath: { type: String },
-    createdAt: { type: Date, default: Date.now }
-});
+// const InspectionSchema = new mongoose.Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//     originalImage: { type: String, required: true },
+//     processedImage: { type: String },
+//     crackCount: { type: Number, default: 0 },
+//     crackSeverity: { type: String },
+//     confidenceScore: { type: Number },
+//     crackType: { type: String },
+//     recommendation: { type: String },
+//     reportPath: { type: String },
+//     createdAt: { type: Date, default: Date.now }
+// });
 
-module.exports = mongoose.model('Inspection', InspectionSchema);
+// module.exports = mongoose.model('Inspection', InspectionSchema);
