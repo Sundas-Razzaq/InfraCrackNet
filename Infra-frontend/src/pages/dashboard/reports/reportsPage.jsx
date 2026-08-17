6266718398ecd960a8addac4ab210a5001d545ec
Sundas-Razzaq <sundasr621@gmail.com>
2026-08-17 16:47:08 +0500

    vercel logs error

--- Infra-frontend/src/pages/dashboard/reports/reportsPage.jsx
+++ Infra-frontend/src/pages/dashboard/reports/reportsPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import EmptyState from "../../../components/dashboard/shared/EmptyState";
-import ReportCard from "../../../components/reports/ReportCard";
+import ReportCard from "../../../components/reports/reportCard";
 import { getAllReports } from "../../../api/reportApi";
 import { getApiErrorMessage } from "../../../api/authApi";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
